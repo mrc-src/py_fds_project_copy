17116d408b431d4d7bfe884de68500ea6672652a
usec123 <marcello_k@abv.bg>
2026-04-26 23:42:46 +0300

    moved run.cmd to main directory

--- run.cmd
+++ run.cmd
@@ -3,9 +3,10 @@ SET /p filename=Enter the input file name (leave blank for 'crates.txt'):
 IF "%filename%"=="" (
 	SET filename=crates.txt
 )
-IF EXIST ".\%filename%" (
+IF EXIST ".\py_fds_copy\%filename%" (
 	ECHO Running script for file '%filename%'
 	ECHO.
+	CD py_fds_copy
 	py -m main -filename %filename%
 ) ELSE (
 	ECHO File does not exist
